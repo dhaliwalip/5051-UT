3a18fd732c1ed72fd5966301f6931be97992b444
Parabhpreet Dhaliwal <dhaliwalpara@seattleu.edu>
2019-06-11 13:04:05 -0700

    Added ReportView Model Instantiante default should pass.

---
+++ UnitTests/Models/ReportViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class ReportViewModel
+    {
+        [TestMethod]
+        public void ReportViewModel_Instantiate_Default_Should_Pass()
+        {
+        //Arrange
+
+        //Act
+
+        //Assert
+
+
+        }
+    }
+}
