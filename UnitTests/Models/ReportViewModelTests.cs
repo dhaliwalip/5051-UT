99a1d1330c0c06406f69c4d34a8751f612391227
Parabhpreet Dhaliwal <dhaliwalpara@seattleu.edu>
2019-06-11 20:30:44 -0700

    Added ReportViewModel_LogViewModel_Set_Default_Should_Pass()

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -47,6 +47,39 @@ namespace UnitTests.Models
             Assert.AreNotEqual(myTestNumberOfUsers, myTest.NumberOfUsers);
             Assert.AreEqual(myTest.NumberOfUsers, 7);
         }
+
+        [TestMethod]
+        public void ReportViewModel_LogViewModel_Get_Default_Should_Pass()
+        {
+            //Arrange
+            var myTest = new ReportViewModel();
+
+            //Act
+            var result = myTest.LogViewModel;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void ReportViewModel_LogViewModel_Set_Default_Should_Pass()
+        {
+            //Arrange
+            var myTest = new ReportViewModel();
+
+            var myLog = new LogModel();
+            var myPhoneId = "Phone";
+
+
+            //Act
+            myLog.PhoneID = myPhoneId;
+            var result = myLog.PhoneID;
+
+            //Assert
+            Assert.AreEqual("Phone", result);
+            
+        }
+
     }
 }
 
